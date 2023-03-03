25e36abf7077947f67e4fc151796dbf8cb4862f0
Slavinskiy Alexander <knownout@hotmail.com>
2023-03-03 13:06:16 +0200

    Update webpack configuration

--- webpack.package.js
+++ webpack.package.js
@@ -15,10 +15,37 @@ const packageConfig = Object.assign(defaultConfig, {
 
     entry: {
         lib: path.resolve(__dirname, "package", "modal-window"),
-        "src/modal-window-controller": path.resolve(__dirname, "package", "bin", "classes", "DateConverter")
+        "src/modal-window-controller": path.resolve(__dirname, "package", "src", "modal-window-controller.ts"),
+        "src/ModalWindow/index": path.resolve(__dirname, "package", "src", "ModalWindow", "index.ts")
     },
 
-    plugins: []
+    externals: {
+        "mobx": {
+            commonjs: "mobx",
+            commonjs2: "mobx",
+            amd: "mobx"
+        },
+        "mobx-react-lite": {
+            commonjs: "mobx-react-lite",
+            commonjs2: "mobx-react-lite",
+            amd: "mobx-react-lite"
+        },
+        "react": {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "react"
+        },
+        "react-dom": {
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "react-dom"
+        },
+        "@knownout/lib": {
+            commonjs: "@knownout/lib",
+            commonjs2: "@knownout/lib",
+            amd: "@knownout/lib"
+        }
+    }
 });
 
 packageConfig.module.rules[1].use.options.configFile = "tsconfig.package.json";
